bb9636f7e6ad49ae7a3d891ce3603cde0cabc2e6
Rafael Carvalho <rapha.pse@outlook.com>
2019-11-10 14:00:21 -0300

    New API with Kafka - MongoDB

---
+++ library/src/main/java/com/library/io/library/controllre/UserController.java
@@ -0,0 +1,12 @@
+package main.java.com.library.io.library.controllre;
+
+/**
+ * UserController
+ */
+@RestController
+@RequestMapping(value = "kafka")
+public class UserController {
+
+    @Ge
+    
+}
