a66a2a58fde8311cb310f8ec8dc1edfc1a3dee67
Rafael Carvalho Caetano <rapha.pse@outlook.com>
2019-11-16 23:44:18 -0200

    Finished Get

--- redis/src/main/java/com/redis/RedisApplication.java
+++ redis/src/main/java/com/redis/RedisApplication.java
@@ -1,6 +1,5 @@
 package com.redis;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,8 +17,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RedisApplication implements CommandLineRunner {
 
 
-	@Autowired
-	private HotelRepository hotelRepository;
+	// @Autowired
+	// private HotelRepository hotelRepository;
 
 
 	public static void main(String[] args) {
