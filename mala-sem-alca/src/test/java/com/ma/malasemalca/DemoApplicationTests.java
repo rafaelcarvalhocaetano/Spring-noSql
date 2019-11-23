beb92e95ecf2f4ee2c9de23a47b57a4976c92d2f
Rafael Carvalho Caetano <rapha.pse@outlook.com>
2019-11-23 13:59:15 -0200

    Projeto mala sem alça

---
+++ mala-sem-alca/src/test/java/com/ma/malasemalca/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ma.malasemalca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
