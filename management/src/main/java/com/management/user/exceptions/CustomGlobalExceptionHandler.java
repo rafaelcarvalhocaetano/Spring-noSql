f9629d2adda2b3f1ac7438086f75f3f486a03ea7
Rafael Carvalho Caetano <rapha.pse@outlook.com>
2019-12-01 20:51:25 -0200

    New Exception

--- management/src/main/java/com/management/user/exceptions/CustomGlobalExceptionHandler.java
+++ management/src/main/java/com/management/user/exceptions/CustomGlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -17,13 +18,24 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
   protected ResponseEntity<Object> handleMethodArgumentNotValid (
     MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request
   ) {
-    // return handleExceptionInternal(ex, null, headers, status, request);
     CustomErrorDetails details = new CustomErrorDetails(
       new Date(), 
       "ERRO BLABLABLAAAAAA",
       ex.getMessage()
       );
-
     return new ResponseEntity<Object>(details, HttpStatus.BAD_REQUEST);
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(
+    HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status,
+    WebRequest request
+  ) {
+    CustomErrorDetails details = new CustomErrorDetails(
+      new Date(), 
+      "GET ERRO BLABLABLABLA",
+      ex.getMessage()
+      );
+      return new ResponseEntity<Object>(details, HttpStatus.METHOD_NOT_ALLOWED);
   }
 }
