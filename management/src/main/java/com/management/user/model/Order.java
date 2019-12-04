9947529bbe9aae79feb786820320bd9e90302db2
Rafael Carvalho Caetano <rapha.pse@outlook.com>
2019-12-04 20:13:43 -0200

    Fix

--- management/src/main/java/com/management/user/model/Order.java
+++ management/src/main/java/com/management/user/model/Order.java
@@ -17,7 +17,7 @@ public class Order implements Serializable {
   private static final long serialVersionUID = 1L;
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private Long orderId;
 
   private String orderDescription;
