187988676db152687cc9ad6277599174f3ddabb1
Rafael Carvalho <rapha.pse@outlook.com>
2020-01-31 22:06:48 -0300

    API-SW with data base MongoDB

---
+++ api-sw/src/main/java/com/apisw/SWApplication.java
@@ -0,0 +1,13 @@
+package com.apisw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SWApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SWApplication.class, args);
+	}
+
+}
---
+++ api-sw/src/test/java/com/apisw/SWApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apisw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SWApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
