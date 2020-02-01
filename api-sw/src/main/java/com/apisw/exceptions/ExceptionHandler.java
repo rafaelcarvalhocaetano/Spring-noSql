927057c3ffdf857164777f054dfce066efcfcb48
Rafael Carvalho <rapha.pse@outlook.com>
2020-02-01 09:26:05 -0300

    New Anottation

--- api-sw/src/main/java/com/apisw/exceptions/ExceptionHandler.java
+++ api-sw/src/main/java/com/apisw/exceptions/ExceptionHandler.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.apisw.exceptions.UserException;
 
@@ -12,10 +13,11 @@ import com.apisw.exceptions.UserException;
 public class ExceptionHandler {
 
 
+  @ExceptionHandler(UserException.class)
   public ResponseEntity<StandardError> objectNotFound(UserException e, HttpServletRequest http) {
 
-    HttpStatus status = HttpStatus.NOT_FOUND;
-    StandardError error = new StandardError(System.currentTimeMillis(), status.value(), "Não Encontrado", e.getMessage(), http.getRequestURI());
+    final HttpStatus status = HttpStatus.NOT_FOUND;
+    final StandardError error = new StandardError(System.currentTimeMillis(), status.value(), "Não Encontrado", e.getMessage(), http.getRequestURI());
     return ResponseEntity.status(status).body(error);
   
   }
